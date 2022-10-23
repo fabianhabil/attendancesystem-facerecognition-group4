45b6cf31613c02149ad649e3f8c97bd99583ceef
fabianhabil <fabianhabilramdhan@gmail.com>
2022-10-24 01:13:34 +0700

    initiated backend with django and requirements module

--- pre https server/image_comparison.py
+++ pre https server/image_comparison.py
@@ -1,15 +1,15 @@
 import cv2
 import face_recognition
 
-img = cv2.imread("images/Ghassan.jpg")
+img = cv2.imread("images/Ghassan-1.jpg")
 rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_encoding = face_recognition.face_encodings(rgb_img)[0]
 
-img2 = cv2.imread("images/Fabian Habil.jpg")
+img2 = cv2.imread("images/Ghassan-2.jpg")
 rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
 img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
 
 result = face_recognition.compare_faces([img_encoding], img_encoding2)
-print("Result: ", result)
+print("Result: ", result[0])
 
 cv2.waitKey(0)

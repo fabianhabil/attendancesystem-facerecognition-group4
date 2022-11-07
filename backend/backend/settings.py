45c0782f835eb28b9e51d1e92d4eadba25ac661d
ASUS <joshuawenata@gmail.com>
2022-11-07 22:15:07 +0700

    database migration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
     "rest_framework",
-    "base"
+    "base",
+    "api"
 ]
 
 MIDDLEWARE = [
@@ -77,9 +77,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'attendance_ai',
+    'USER': 'root',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '3306',
     }
 }
 
--- backend/base/models.py
+++ backend/base/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 # Create your models here.
-class Item(models.Model):
-    name = models.CharField(max_length=200)
+
+class Image(models.Model):
+    image  = models.ImageField(upload_to='images')
+
+class User(models.Model):
+    fullname = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
+    id_image = models.ForeignKey(Image, on_delete=models.CASCADE)
     

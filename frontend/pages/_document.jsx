0c728838301b57cd9dd7aceb519ff649556f10fd
fabianhabil <fabianhabilramdhan@gmail.com>
2022-12-11 03:31:33 +0700

    installed new font

--- frontend/pages/_document.jsx
+++ frontend/pages/_document.jsx
@@ -13,9 +13,11 @@ export default class MyDocument extends Document {
                     {/* PWA primary color */}
                     <meta name='theme-color' content={theme.palette.primary.main} />
                     <link rel='shortcut icon' href='/static/favicon.ico' />
+                    <link rel='preconnect' href='https://fonts.googleapis.com' />
+                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
                     <link
+                        href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap'
                         rel='stylesheet'
-                        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
                     />
                     {/* Inject MUI styles first to match with the prepend: true configuration. */}
                     {this.props.emotionStyleTags}
--- frontend/theme/theme.jsx
+++ frontend/theme/theme.jsx
@@ -13,6 +13,21 @@ const theme = createTheme({
         error: {
             main: red.A400
         }
+    },
+    typography: {
+        fontFamily: [
+            'Inter',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"'
+        ].join(',')
     }
 });
 
